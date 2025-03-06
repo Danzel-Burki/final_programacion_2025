30af3071b0109679c1a585be598b38a570dd76c4
DanzelBurki <168141661+Danzel-Burki@users.noreply.github.com>
2025-03-06 19:42:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de Libros</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a la Gestión de Libros</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="./php/abm_libros.php">Gestión de Libros</a></li>
+            <li><a href="./php/consulta_libros.php">Consultar Libros Disponibles</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
